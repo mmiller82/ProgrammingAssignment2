d8cdacc8e0e3ede4ed0de9d3cb7a198803f40b3b
Maury Miller <maury.miller@ieee.org>
2014-06-21 07:42:15 -0700

    My solution to programming assignment 2.  It has two functions. The first one use the <-- operator.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## This function creates a list of functions that stores a matrix and its
+## inverse.
+## The function uses the <<- operator to make internal variables accessible.
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) i <<- solve
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function sets the inverse matrix in the list created by makeCacheMatrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    ## Check to see whether matrix is invertible
+    if (det(data) == 0) {
+        message("matrix is noninvertible")
+        return
+    } else {
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
+    }
 }
